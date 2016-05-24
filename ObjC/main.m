f3f6c2b24defa5574326788b6019f58d16c23b08
Naeun Shin <naeun.shin@samsung.com>
2016-05-24 11:02:35 +0900

    Initial Commit

---
+++ ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by MF839-007 on 2016. 5. 24..
+//  Copyright © 2016년 MF839-007. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
